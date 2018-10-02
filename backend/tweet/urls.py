d2097c57df884a1476c3cabe4738fe4144a663a4
Le Duy Khanh <ledu0003@e.ntu.edu.sg>
2018-10-02 10:39:53 +0800

    auth fk

--- backend/tweet/urls.py
+++ backend/tweet/urls.py
@@ -2,9 +2,23 @@ from django.urls import path
 
 from . import views
 
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'm', views.MessageViewSet)
+
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+
+]
+
 urlpatterns = [
     path('data', views.data, name='data'),
     path('add_message', views.add_message, name='add_message'),
-    path('translate', views.translate, name='translate'),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', include(router.urls))
 ]
--- backend/tweet/views.py
+++ backend/tweet/views.py
@@ -3,10 +3,48 @@ from django.http import HttpResponse, JsonResponse
 from .models import *
 from .serializers import *
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets
+from rest_framework.decorators import action
 import json
 
 from .lib.snippets import translate_text
 # Create your views here.
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = MessageSerializzer
+    queryset = Message.objects.all()
+
+    def list(self, request):
+        data_list = Message.objects.order_by('-time_added')[:10]
+        t_list = []
+        for t in Translate.objects.all()[:10]:
+            text = str(t.user) + ' translated ' + t.translated_text
+            t_list.append(MessageSerializzer(Message(text=text, time_added=t.time_added)).data)
+        return JsonResponse({'data':MessageSerializzer(data_list, many=True).data + t_list})
+
+    @action(methods=['post'], detail=False)
+    def translate(self, request):
+        data = json.loads(request.body.decode('utf-8'))
+        user = request.user
+        if (user.balance == 0):
+            return JsonResponse({'message': 'NO_BALANCE'}, status=400)
+        user.translates_done = user.translates_done + 1
+        user.balance = user.balance - 1
+        user.save()
+        target = str(data.get("target"))
+        text = str(data.get("text"))
+        result = translate_text(target, text)
+        trans = Translate(text=text,
+                user=user,
+                language =result["detectedSourceLanguage"],
+                target = target,
+                translated_text = result['translatedText'])
+        trans.save()
+        return JsonResponse({'data':result['translatedText'], 'language': result["detectedSourceLanguage"]})
+
 
 def index(request):
     return HttpResponse("home")
@@ -27,22 +65,3 @@ def add_message(request):
         m.save()
     data_list = Message.objects.order_by('-time_added')[:10]
     return JsonResponse({'data':MessageSerializzer(data_list, many=True).data})
-
-def translate(request):
-    data = json.loads(request.body.decode('utf-8'))
-    user = request.user
-    if (user.balance == 0):
-        return JsonResponse({'message': 'NO_BALANCE'}, status=400)
-    user.translates_done = user.translates_done + 1
-    user.balance = user.balance - 1
-    user.save()
-    target = str(data.get("target"))
-    text = str(data.get("text"))
-    result = translate_text(target, text)
-    trans = Translate(text=text,
-            user=user,
-            language =result["detectedSourceLanguage"],
-            target = target,
-            translated_text = result['translatedText'])
-    trans.save()
-    return JsonResponse({'data':result['translatedText'], 'language': result["detectedSourceLanguage"]})
