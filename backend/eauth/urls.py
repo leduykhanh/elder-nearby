4851bbce2090e14d907e4e1a4b652a9a94178b99
Le Duy Khanh <ledu0003@e.ntu.edu.sg>
2018-10-22 15:06:01 +0800

    demo_login

--- backend/eauth/urls.py
+++ backend/eauth/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('register_by_access_token/<slug:backend>/', views.register_by_access_token, name='login'),
+    path('demo_login', views.demo_login, name='demo_login')
 ]
--- backend/eauth/views.py
+++ backend/eauth/views.py
@@ -26,7 +26,22 @@ def register_by_access_token(request, backend):
         try:
             key = Token.objects.create(user=user).key
         except:
-            key = Token.objects.get(user=user).key    
+            key = Token.objects.get(user=user).key
         return JsonResponse({"data": CustomUserDetailsSerializer(user).data, "key": key})
     else:
         return 'ERROR'
+
+def demo_login(request):
+    return JsonResponse(
+            {
+            "display_name": "Demo User",
+            "email": "demo@gmail.com",
+            "eth_balance": 0,
+            "tkx_balance": 0,
+            "swapped_timestamp": null,
+            "swapped_status": 0,
+            "start_timestamp": 1526662800,
+            "end_timestamp": 1526687999,
+            "auth_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRvcmlubmd1eWVuQGdtYWlsLmNvbSIsImV4cGlyZXNJbiI6MTU4NDcxNzA1Njk2MSwiaWF0IjoxNTI0NzE3MDU2LCJleHAiOjE1ODYyNDE3NzQwMTd9.ncILPSMzuU2gz3KpGh4GBoUC5-R0_Z-zr6ZHd-HAa18"
+        }
+    )
